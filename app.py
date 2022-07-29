e5d6ab8b8555e4120fe5f463c7516dc9a1b6aaad
linusschlepp <linusmo.94@googlemail.com>
2022-07-29 17:55:06 +0200

    Added contactform

--- app.py
+++ app.py
@@ -1,18 +1,42 @@
 import datetime
-
-from flask import Flask, render_template
+import requests
+from forms import ContactForm
+from flask import Flask, render_template, request
 from dateutil.relativedelta import relativedelta
+import os
 
 app = Flask(__name__)
+SECRET_KEY = os.urandom(32)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+
+@app.route('/', methods=['POST'])
+def contact_me():
+    form = ContactForm()
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        subject = request.form['subject']
+    #  send_mail(subject=subject, message=message)
+    #  return render_template('thankyou.html')
+    else:
+        return render_template('index.html', form=form)
 
 
 def year_delta():
-    print(type(relativedelta(datetime.datetime.now(), datetime.datetime(2000,6,14,0,0,0,0))))
-    return str(relativedelta(datetime.datetime.now(), datetime.datetime(2000,6,14,0,0,0,0)).years)
+    print(type(relativedelta(datetime.datetime.now(), datetime.datetime(2000, 6, 14, 0, 0, 0, 0))))
+    return str(relativedelta(datetime.datetime.now(), datetime.datetime(2000, 6, 14, 0, 0, 0, 0)).years)
+
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html', age=year_delta())
+    form = ContactForm()
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+        subject = request.form['subject']
+    return render_template('index.html', age=year_delta(), form=form)
+
 
 @app.route('/test')
 def test():  # put application's code here
